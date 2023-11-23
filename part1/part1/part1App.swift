0e959c10cb462dc5774fb0e8da6cc2c2e97580f5
seungdeok <akk9132@naver.com>
2023-11-23 21:20:51 +0900

    init project

---
+++ part1/part1/part1App.swift
@@ -0,0 +1,17 @@
+//
+//  part1App.swift
+//  part1
+//
+//  Created by 정승덕 on 2023/11/23.
+//
+
+import SwiftUI
+
+@main
+struct part1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
